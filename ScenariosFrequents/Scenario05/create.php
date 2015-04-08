82be4b628a0836e05eee77f340acc534c26d2876
Carl-Hugo Marcotte <carlhugo@gmail.ca>
2015-04-08 11:11:19 -0400

    Scénario 5

---
+++ ScenariosFrequents/Scenario05/create.php
@@ -0,0 +1,38 @@
+<?php
+if (isset($_POST['nom'], $_POST['prenom']))
+{
+    // Traitement et validation ici ...
+    header('Location:index.php');
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Scénario 5 - Création d'un enregistrement</title>
+    </head>
+    <body>
+        <h1>
+            POST --> GET: Envoi d'un formulaire de création et 
+            redirection vers la liste.
+        </h1>
+        <?php
+        // put your code here
+        ?>
+        <form method="POST">
+            <div>
+                <label>
+                    Nom:
+                    <input type="text" name="nom">
+                </label>
+            </div>
+            <div>
+                <label>
+                    Prénom:
+                    <input type="text" name="prenom">
+                </label>
+            </div>
+            <button type="submit">POST!</button>
+        </form>
+    </body>
+</html>
---
+++ ScenariosFrequents/Scenario05/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Scénario 5 - Liste</title>
+    </head>
+    <body>
+        <a href="create.php">Création d'un enregistrement</a>
+        <ul>
+            <li>Ma</li>
+            <li>Liste</li>
+            <li>De</li>
+            <li>Données</li>
+        </ul>
+    </body>
+</html>
