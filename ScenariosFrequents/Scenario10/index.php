b7e54e00315741bf17955a0268de927b2325f9dc
Carl-Hugo Marcotte <carlhugo@gmail.ca>
2015-04-08 13:32:15 -0400

    Scénario 10

---
+++ ScenariosFrequents/Scenario10/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Scénario 10 - Index</title>
+    </head>
+    <body>
+        <h1>
+            POST --> SESSION --> GET: Connexion dans un système, sauvegarde 
+            en session et redirection.
+        </h1>
+        <?php
+        // put your code here
+        ?>
+        <a href="login.php">Connexion</a>
+    </body>
+</html>
